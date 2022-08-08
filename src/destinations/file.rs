28c1ed5e4cf0303b41e1fe880e47bfc9f7beb0e6
James <jameshendry05@gmail.com>
2022-08-08 19:21:03 +0100

    Cut down to only millisecond precision

--- src/destinations/file.rs
+++ src/destinations/file.rs
@@ -5,7 +5,7 @@ use serde::{Serialize, Deserialize};
 use std::fmt::{Debug, Display, Write};
 use std::fs;
 use std::io::Write as IoWrite;
-use chrono::TimeZone;
+use chrono::{SecondsFormat, TimeZone};
 use crate::destinations::MessageDestination;
 use crate::message::Message;
 
@@ -44,12 +44,12 @@ impl FileDestination {
     fn format_message<TZ: TimeZone>(&self, message: &Message<TZ>) -> String
         where TZ::Offset: Display {
         let mut s = String::new();
-        write!(s, "{} - {:?}:", message.get_timestamp().to_rfc3339(), message.get_level()).unwrap();
+        write!(s, "{} - {:?}: ", message.get_timestamp().to_rfc3339_opts(SecondsFormat::Millis, true), message.get_level()).unwrap();
         if message.get_component().is_some() {
-            write!(s, " [{}] ", message.get_component().as_ref().unwrap()).unwrap();
+            write!(s, "[{}] ", message.get_component().as_ref().unwrap()).unwrap();
         }
         if message.get_title().is_some() {
-            write!(s, " {} - ", message.get_title().as_ref().unwrap()).unwrap();
+            write!(s, "{} - ", message.get_title().as_ref().unwrap()).unwrap();
         }
         write!(s, "'{}'", message.get_message_detail()).unwrap();
         if message.get_author().is_some() {
