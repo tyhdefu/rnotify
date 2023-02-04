66c6f650019636f26c12e9415a241d3ab1b3a610
James <jameshendry05@gmail.com>
2023-02-04 21:29:11 +0000

    Add proper formatting for mail

--- src/destination/kinds/mail.rs
+++ src/destination/kinds/mail.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
-use lettre::message::Mailbox;
+use lettre::message::{Mailbox, SinglePart};
 use lettre::{SmtpTransport, Transport};
 use lettre::transport::smtp::authentication;
 use serde::{Serialize, Deserialize};
 use crate::destination::{MessageDestination, SerializableDestination};
-use crate::message::Message;
+use crate::message::{Message, MessageDetail};
+use crate::message::formatted_detail::{FormattedMessageComponent, FormattedMessageDetail, FormattedString, Style};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct MailDestination {
@@ -43,7 +44,7 @@ impl MessageDestination for MailDestination {
             message_builder = message_builder.subject(title);
         }
 
-        let email = message_builder.body(message.get_message_detail().raw().to_owned())?;
+        let email = message_builder.singlepart(create_body(message.get_message_detail()))?;
 
         let creds = authentication::Credentials::new(self.relay.username.clone(), self.relay.password.clone());
         let mailer =
@@ -61,5 +62,93 @@ impl MessageDestination for MailDestination {
 impl SerializableDestination for MailDestination {
     fn as_message_destination(&self) -> &dyn MessageDestination {
         self
+    }
+}
+
+fn create_body(detail: &MessageDetail) -> SinglePart {
+    match detail {
+        MessageDetail::Raw(raw) => {
+            SinglePart::plain(raw.to_owned())
+        }
+        MessageDetail::Formatted(formatted) => {
+            SinglePart::html(formatted_to_html(formatted))
+        }
+    }
+}
+
+fn formatted_to_html(formatted: &FormattedMessageDetail) -> String {
+    let mut html = String::with_capacity(100);
+    for component in formatted.components() {
+        match component {
+            FormattedMessageComponent::Section(section, formatted_string) => {
+                html.push_str(&format!("<div><h2>{}</h2><p>{}</p></div>",
+                                       escape_html(section),
+                                       parse_formatted(formatted_string)
+                ));
+            }
+            FormattedMessageComponent::Text(formatted_string) => {
+                html.push_str(&format!("<p>{}</p>", parse_formatted(formatted_string)))
+            }
+        }
+    }
+    html
+}
+
+fn escape_html(s: &str) -> String {
+    s.replace('>', "&lt;")
+        .replace('<', "&gt;")
+        .replace('&', "&amp;")
+}
+
+fn parse_formatted(formatted: &Vec<FormattedString>) -> String {
+    let mut html = String::new();
+    for part in formatted {
+        for style in part.get_styles() {
+            let start_tag = match style {
+                Style::Bold => "<b>",
+                Style::Italics => "<i>",
+                Style::Monospace => "<code>",
+                Style::Code { lang: _ } => "<code>",
+            };
+            html.push_str(start_tag);
+        }
+        html.push_str(&escape_html(part.get_string()));
+        for style in part.get_styles() {
+            let end_tag = match style {
+                Style::Bold => "</b>",
+                Style::Italics => "</i>",
+                Style::Monospace => "</code>",
+                Style::Code { lang: _ } => "</code>",
+            };
+            html.push_str(end_tag);
+        }
+    }
+    html
+}
+
+#[cfg(test)]
+mod test {
+    use crate::message::detail_builder::{FormattedStringAppendable, MessageDetailBuilder};
+    use crate::message::MessageDetail::Formatted;
+    use super::*;
+
+    #[test]
+    fn test_html_conversion() {
+        let mut builder = MessageDetailBuilder::new();
+        builder.section("hello world", |body| {
+            body.append_plain("Dear fellow inhabitants. it has come to my attention that ");
+            body.append_styled("you have not been doing your job.", Style::Bold);
+        });
+        builder.text_block(|block| {
+            block.append_plain("That is all.");
+        });
+        let message = builder.build();
+        if let Formatted(formatted_detail) = message {
+            let html = formatted_to_html(&formatted_detail);
+            assert_eq!(html, "<div><h2>hello world</h2><p>Dear fellow inhabitants. it has come to my attention that <b>you have not been doing your job.</b></p></div><p>That is all.</p>")
+        }
+        else {
+            panic!("oops");
+        }
     }
 }
